4615a059715a38881cfbc2212f6f5aa817f39689
Rei <chloerei@gmail.com>
2010-12-03 19:22:00 +0000

    add blogbus importer.

--- lib/blog_converter.rb
+++ lib/blog_converter.rb
@@ -4,6 +4,7 @@ require 'blog_converter/article'
 require 'blog_converter/comment'
 require 'blog_converter/document'
 require 'blog_converter/importer/wordpress'
+require 'blog_converter/importer/blogbus'
 require 'blog_converter/exporter/wordpress'
 
 module BlogConverter
